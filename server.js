3938cff9afc49faeb374d5f7b568b73f478d8066
Yegor <kes1993@ya.ru>
2016-04-27 18:46:41 +0600

    te

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
-function Username (name) {
-	this.name=name;
-}
-Username.prototype.hello = function(who) {
-	console.log("hello" + who.name);
-};
+require('./user');
+
 var ildar =new Username("Ильдар");
 var hui =new Username("Вадим");
 
---
+++ user.js
@@ -0,0 +1,9 @@
+function Username (name) {
+	this.name=name;
+}
+Username.prototype.hello = function(who) {
+	console.log("hello " + who.name);
+};
+
+
+console.log("User is required");
