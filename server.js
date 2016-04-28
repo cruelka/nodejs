a841353cf2fd50306203c22060ec91dc00a205f3
Yegor <kes1993@ya.ru>
2016-04-28 16:31:32 +0600

    added db

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var Username = require('./user');
 
+function run(){
 var ildar =new Username("Ильдар");
 var hui =new Username("Вадим");
+}
 
+if (module.parent) {
+	exports.run = run;
+} else {
+	run();
+}
+;
 ildar.hello(hui);
