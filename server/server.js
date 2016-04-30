65b168fbc168a25e1aaa7df1e33303d5c814defc
Yegor <kes1993@ya.ru>
2016-04-30 19:29:50 +0600

    11

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var server = new http.Server();
 server.listen("1337, 127.0.0.1");
 
 server.on('request', function(req, res) {
+	res.writeHead(200, {'Content-Type':'text/plain'});
 	res.end("Hello world!");
 
 });
+console.log('Server runned');
