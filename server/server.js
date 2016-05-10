a07eab13df96e600bf69d0f5bc4577413d37cf7d
Yegor <kes1993@ya.ru>
2016-05-10 21:55:56 +0600

    port change

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = new http.Server();
 
-server.listen("1337, 31.31.192.87");
+server.listen("8080, 31.31.192.87");
 
 server.on('request', function(req, res) {
 	res.writeHead(200, {'Content-Type':'text/plain'});
